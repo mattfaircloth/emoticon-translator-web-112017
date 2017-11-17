10f4fc04fc79b935ad943ea53c1a8bf07bef4573
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-17 15:38:21 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon(file, emoticon)
   end
 end
 
-def get_english_meaning
+def get_english_meaning(file, emoticon)
   emoticons = load_library(file)
 
   answer = emoticons["get_meaning"][emoticon]
